90d89ff92ed7aaa72053d37010454c3f15f37bac
Traian Rusu <trusu@customtruck.com>
2020-08-20 16:26:22 -0400

    added some overall styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
+import Controls from './components/Controls';
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trading View Watchlist Generator</h1>
       </header>
+      <main>
+        <h2>Select from the exchanges below</h2>
+        <Controls />
+      </main>
     </div>
   );
 }
