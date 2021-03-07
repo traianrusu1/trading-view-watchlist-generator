c6ec32c17b17ff228fdb0619d23723e9f0486024
Traian Rusu <traianrusu1@gmail.com>
2021-03-07 14:43:58 -0500

    change layout and styling of items

--- src/components/Trending/TrendingListItem.tsx
+++ src/components/Trending/TrendingListItem.tsx
@@ -28,68 +28,66 @@ function TrendingListItem({ coinData }: Props): ReactElement {
       </div>
       <div className={styles.itemTitle}>{coinData.trendingData.name}</div>
 
-      <TrendingListItemLine label="Cap Rank" value={coinData.generalData.market_cap_rank} />
-      <TrendingListItemLine
-        label="CoinGecko Score"
-        value={coinData.generalData.coingecko_score?.toFixed(2)}
-      />
-      <TrendingListItemLine
-        label="Community Score"
-        value={coinData.generalData.community_score?.toFixed(2)}
-      />
-      <TrendingListItemLine
-        label="Liquidity Score"
-        value={coinData.generalData.liquidity_score?.toFixed(2)}
-      />
-      <TrendingListItemLine
-        label="Interest Score"
-        value={coinData.generalData.public_interest_score?.toFixed(2)}
-      />
-      <TrendingListItemLine
-        label="Up Votes"
-        value={coinData.generalData.sentiment_votes_up_percentage?.toFixed(2)}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Down Votes"
-        value={coinData.generalData.sentiment_votes_down_percentage?.toFixed(2)}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Last 24 Hours"
-        value={coinData.generalData.market_data.price_change_percentage_24h_in_currency?.btc?.toFixed(
-          2,
-        )}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Last 7 Days"
-        value={coinData.generalData.market_data.price_change_percentage_7d_in_currency?.btc?.toFixed(
-          2,
-        )}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Last 14 Days"
-        value={coinData.generalData.market_data.price_change_percentage_14d_in_currency?.btc?.toFixed(
-          2,
-        )}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Last 30 Days"
-        value={coinData.generalData.market_data.price_change_percentage_30d_in_currency?.btc?.toFixed(
-          2,
-        )}
-        isPercentage
-      />
-      <TrendingListItemLine
-        label="Last 60 Days"
-        value={coinData.generalData.market_data.price_change_percentage_60d_in_currency?.btc?.toFixed(
-          2,
-        )}
-        isPercentage
-      />
+      <section className={styles.dataGroup}>
+        <h3 className={styles.groupHeading}>Rank & Score</h3>
+        <TrendingListItemLine label="Cap Rank" value={coinData.generalData.market_cap_rank} />
+        <TrendingListItemLine
+          label="CoinGecko Score"
+          value={coinData.generalData.coingecko_score?.toFixed(2)}
+        />
+        <TrendingListItemLine
+          label="Community Score"
+          value={coinData.generalData.community_score?.toFixed(2)}
+        />
+        <TrendingListItemLine
+          label="Up Votes"
+          value={coinData.generalData.sentiment_votes_up_percentage?.toFixed(2)}
+          isPercentage
+        />
+        <TrendingListItemLine
+          label="Down Votes"
+          value={coinData.generalData.sentiment_votes_down_percentage?.toFixed(2)}
+          isPercentage
+        />
+      </section>
+      <section className={styles.dataGroup}>
+        <h3>Price Change (BTC)</h3>
+        <TrendingListItemLine
+          label="Last 24 Hours"
+          value={coinData.generalData.market_data.price_change_percentage_24h_in_currency?.btc?.toFixed(
+            2,
+          )}
+          isPercentage
+        />
+        <TrendingListItemLine
+          label="Last 7 Days"
+          value={coinData.generalData.market_data.price_change_percentage_7d_in_currency?.btc?.toFixed(
+            2,
+          )}
+          isPercentage
+        />
+        <TrendingListItemLine
+          label="Last 14 Days"
+          value={coinData.generalData.market_data.price_change_percentage_14d_in_currency?.btc?.toFixed(
+            2,
+          )}
+          isPercentage
+        />
+        <TrendingListItemLine
+          label="Last 30 Days"
+          value={coinData.generalData.market_data.price_change_percentage_30d_in_currency?.btc?.toFixed(
+            2,
+          )}
+          isPercentage
+        />
+        <TrendingListItemLine
+          label="Last 60 Days"
+          value={coinData.generalData.market_data.price_change_percentage_60d_in_currency?.btc?.toFixed(
+            2,
+          )}
+          isPercentage
+        />
+      </section>
     </article>
   );
 }
